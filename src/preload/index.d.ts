2b22a5cc8a9f5853a263b7a8b88bc5afe30184e1
Aimony <aimony@qq.com>
2025-12-10 13:56:20 +0800

    feat: Add global Electron and custom API type declarations for preloaded scripts.

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,9 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
+import type { ElectronApi } from '../renderer/src/types/electron'
 
 declare global {
   interface Window {
     electron: ElectronAPI
-    api: unknown
+    api: ElectronApi
   }
 }
